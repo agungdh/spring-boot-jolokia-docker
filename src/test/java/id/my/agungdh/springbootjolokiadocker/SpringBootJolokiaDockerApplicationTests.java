66a989073eaa579c0dd4404bca18b90de6bc9e39
AgungDH <agungdh@live.com>
2025-05-05 23:20:04 +0700

    initialize Spring Boot application with Maven wrapper and basic test setup

---
+++ src/test/java/id/my/agungdh/springbootjolokiadocker/SpringBootJolokiaDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.springbootjolokiadocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJolokiaDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
